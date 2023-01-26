47f0552405a0df1f5430be3e36646836cb0cce82
kunal Jha <kunaljha331@gmail.com>
2023-01-26 14:40:03 +0530

    fifth commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,12 @@
 import json
+import pandas as pd
 
 def lambda_handler(event, context):
     # TODO implement
+    d={'col1':[1,2],'col2':[3,4]}
+    df=pd.DataFrame(data=d)
+    print(df)
+    print('done')
     print(event)
     print(context)
     return {
